3574ea3b8fba19791a534bbf0105a9a23aaab5c2
asom <86374869+as0mdude@users.noreply.github.com>
2024-11-06 21:52:01 -0800

    l16 dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const container = document.getElementById('button-container') || document.body;
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+button.addEventListener('click', () => {
+  alert('You clicked the button!');
+});
+
+container.appendChild(button);
